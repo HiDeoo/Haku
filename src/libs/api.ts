b58136fc970d42c5a3e2041c78c84ed13f385cf2
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-12-07 18:04:41 +0100

    Fix various issues with the email allowed list API

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -9,7 +9,10 @@ export function createApiRoute<Get, Post, Put, Delete, Patch>(
 ) {
   const orderedMiddlewares = middlewares?.reverse()
 
-  return async (req: NextApiRequest, res: NextApiResponse<Get | Post | Put | Delete | Patch | ApiErrorResponse>) => {
+  return async (
+    req: NextApiRequest,
+    res: NextApiResponse<Get | Post | Put | Delete | Patch | ApiClientErrorResponse>
+  ) => {
     let handler: NextApiHandler | undefined
 
     if (req.method === HttpMethod.GET && route.get) {
@@ -35,16 +38,13 @@ export function createApiRoute<Get, Post, Put, Delete, Patch>(
         }
       }
 
-      return handler(req, res)
+      await handler(req, res)
     } catch (error) {
       let statusCode = StatusCode.ServerErrorInternal
       let message = 'Something went wrong!'
 
-      if (error instanceof ApiError) {
-        if (error.options.httpStatusCode) {
-          statusCode = error.options.httpStatusCode
-        }
-
+      if (error instanceof ApiClientError) {
+        statusCode = error.httpStatusCode
         message = error.message
       } else {
         console.error('Unhandled API error:', error)
@@ -55,8 +55,8 @@ export function createApiRoute<Get, Post, Put, Delete, Patch>(
   }
 }
 
-export class ApiError extends Error {
-  constructor(public message: string, public options: ApiErrorOptions = {}) {
+export class ApiClientError extends Error {
+  constructor(public message: string, public httpStatusCode: StatusCode = StatusCode.ClientErrorForbidden) {
     super(message)
 
     Object.setPrototypeOf(this, new.target.prototype)
@@ -73,10 +73,6 @@ interface ApiRoute<Get, Post, Put, Delete, Patch> {
 
 type ApiMiddleware = (handler: NextApiHandler) => NextApiHandler
 
-interface ApiErrorOptions {
-  httpStatusCode?: StatusCode
-}
-
-interface ApiErrorResponse {
+interface ApiClientErrorResponse {
   error: string
 }
--- src/libs/db/emailAllowList.ts
+++ src/libs/db/emailAllowList.ts
@@ -1,15 +1,28 @@
 import { EmailAllowList } from '@prisma/client'
 
+import { ApiClientError } from 'libs/api'
 import { prisma } from 'libs/db'
 
 export function getAllowedEmails(): Promise<EmailAllowList[]> {
   return prisma.emailAllowList.findMany()
 }
 
-export function addAllowedEmail(email: EmailAllowList['email']): Promise<EmailAllowList> {
+export async function addAllowedEmail(email: EmailAllowList['email']): Promise<EmailAllowList> {
+  const existingEmail = await prisma.emailAllowList.findUnique({ where: { email } })
+
+  if (existingEmail) {
+    throw new ApiClientError('This email already exists.')
+  }
+
   return prisma.emailAllowList.create({ data: { email } })
 }
 
 export async function removeAllowedEmail(id: EmailAllowList['id']): Promise<void> {
+  const existingEmail = await prisma.emailAllowList.findUnique({ where: { id } })
+
+  if (!existingEmail) {
+    throw new ApiClientError('This email do not exist.')
+  }
+
   await prisma.emailAllowList.delete({ where: { id } })
 }
