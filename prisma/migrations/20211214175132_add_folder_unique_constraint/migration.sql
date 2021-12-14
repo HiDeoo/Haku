1aa066c05a6e39f80971ea1fee214ee24a682308
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-12-14 19:10:36 +0100

    Add folder unique constraint

---
+++ prisma/migrations/20211214175132_add_folder_unique_constraint/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[parentId,type,userId,name]` on the table `Folder` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "Folder_parentId_type_userId_name_unique_constraint" ON "Folder"("parentId", "type", "userId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Folder_parentId_type_userId_name_unique_constraint_with_null_panretId" ON "Folder"("type", "userId", "name") WHERE "parentId" IS NULL;
