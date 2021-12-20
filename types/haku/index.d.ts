864a42fd8f9ec8c1c5b7cfe4c8c96c23aef6dae2
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-12-20 10:22:38 +0100

    Refactor global type definitions

--- types/haku/index.d.ts
+++ types/haku/index.d.ts
@@ -1,3 +1,5 @@
+import { type Session } from 'next-auth'
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
@@ -25,6 +27,11 @@ declare global {
       SHADOW_DB_URL: string
     }
   }
+
+  type UserId = Session['user']['id']
+
+  // Just for the sake of object destructuring and having a better named ID e.g. `const { userId } = user`.
+  type UserWithUserId = Omit<Session['user'], 'id'> & { userId: UserId }
 }
 
 export {}
