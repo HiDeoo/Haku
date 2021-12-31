9fbc7f29cd6aaf15a7dc58bcf3004a5d8a44d65b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-12-31 17:43:12 +0100

    Add content tree routing

--- src/components/ContentTree.tsx
+++ src/components/ContentTree.tsx
@@ -1,4 +1,6 @@
-import { Link, Root } from '@radix-ui/react-toolbar'
+import { Link as Roving, Root } from '@radix-ui/react-toolbar'
+import Link from 'next/link'
+import { forwardRef } from 'react'
 
 import Flex from 'components/Flex'
 import useContentTree from 'hooks/useContentTree'
@@ -6,8 +8,15 @@ import { type FolderData } from 'libs/db/folder'
 import { type NoteData } from 'libs/db/note'
 import { type TodoData } from 'libs/db/todo'
 import { isTreeFolder, type TreeFolder } from 'libs/tree'
+import useContentType, { type ContentType } from 'hooks/useContentType'
 
 const ContentTree: React.FC = () => {
+  const type = useContentType()
+
+  if (!type) {
+    throw new Error('Missing content type to render the content tree.')
+  }
+
   const { data, isLoading } = useContentTree()
 
   // TODO(HiDeoo)
@@ -21,7 +30,11 @@ const ContentTree: React.FC = () => {
         {data?.map((item) => {
           const key = getKey(item)
 
-          return isTreeFolder(item) ? <Folder key={key} folder={item} /> : <Content key={key} content={item} />
+          return isTreeFolder(item) ? (
+            <Folder key={key} folder={item} type={type} />
+          ) : (
+            <Content key={key} content={item} type={type} />
+          )
         })}
       </Root>
     </Flex>
@@ -30,43 +43,63 @@ const ContentTree: React.FC = () => {
 
 export default ContentTree
 
-const Folder: React.FC<FolderProps> = ({ folder }) => {
+const Folder: React.FC<FolderProps> = ({ folder, type }) => {
   return (
     <div>
-      <Link asChild>
+      <Roving asChild>
         <div>FOLDER: {folder.name}</div>
-      </Link>
+      </Roving>
       <div style={{ paddingLeft: 20 }}>
         {folder.children.map((child) => (
-          <Folder key={getKey(child)} folder={child} />
+          <Folder key={getKey(child)} folder={child} type={type} />
         ))}
         {folder.items.map((content) => (
-          <Content key={getKey(content)} content={content} />
+          <Content key={getKey(content)} content={content} type={type} />
         ))}
       </div>
     </div>
   )
 }
 
-const Content: React.FC<ContentProps> = ({ content }) => {
+const Content: React.FC<ContentProps> = ({ content, type }) => {
   return (
-    <Link asChild>
-      <div>FILE: {content.name}</div>
-    </Link>
+    <Roving asChild>
+      <ContentLink href={`/${type.toLowerCase()}s/${content.id}`}>FILE: {content.name}</ContentLink>
+    </Roving>
   )
 }
 
-function getKey(item: FolderType | ContentType): string {
+const ContentLink = forwardRef<HTMLAnchorElement, React.PropsWithChildren<ContentLinkProps>>(
+  ({ children, href, ...props }, ref) => {
+    return (
+      <Link href={href} prefetch={false}>
+        <a ref={ref} {...props}>
+          {children}
+        </a>
+      </Link>
+    )
+  }
+)
+
+ContentLink.displayName = 'ContentLink'
+
+function getKey(item: FolderType | DataType): string {
   return `${isTreeFolder(item) ? 'folder' : 'content'}-${item.id}`
 }
 
 interface FolderProps {
   folder: FolderType
+  type: ContentType
 }
 
 interface ContentProps {
-  content: ContentType
+  content: DataType
+  type: ContentType
 }
 
-type ContentType = NoteData | TodoData
-type FolderType = TreeFolder<FolderData, ContentType>
+interface ContentLinkProps {
+  href: string
+}
+
+type DataType = NoteData | TodoData
+type FolderType = TreeFolder<FolderData, DataType>
