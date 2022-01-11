c3e3f0124ea31a40e410b056cf400b7029e84979
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-01-11 16:17:34 +0100

    Refactor Flex component with useMemo

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react'
+import { forwardRef, useMemo } from 'react'
 
 import clst from 'styles/clst'
 
@@ -20,44 +20,48 @@ const Flex = <Element extends React.ElementType = 'div'>(
 ) => {
   const Element = as || 'div'
 
-  const elementClasses = clst(
-    'flex',
-    {
-      'flex-row': direction === 'row',
-      'flex-row-reverse': direction === 'row-reverse',
-      'flex-col': direction === 'col',
-      'flex-col-reverse': direction === 'col-reverse',
-    },
-    {
-      'flex-1': flex === true,
-      'flex-auto': flex === 'auto',
-      'flex-initial': flex === 'initial',
-      'flex-none': flex === 'none',
-    },
-    {
-      'justify-start': justifyContent === 'start',
-      'justify-end': justifyContent === 'end',
-      'justify-center': justifyContent === 'center',
-      'justify-between': justifyContent === 'between',
-      'justify-around': justifyContent === 'around',
-      'justify-evenly': justifyContent === 'evenly',
-    },
-    {
-      'items-start': alignItems === 'start',
-      'items-end': alignItems === 'end',
-      'items-center': alignItems === 'center',
-      'items-baseline': alignItems === 'baseline',
-      'items-stretch': alignItems === 'stretch',
-    },
-    {
-      'h-full': fullHeight,
-      'w-full': fullWidth,
-    },
-    {
-      'flex-wrap': wrap === true,
-      'flex-wrap-reverse': wrap === 'reverse',
-    },
-    className
+  const elementClasses = useMemo(
+    () =>
+      clst(
+        'flex',
+        {
+          'flex-row': direction === 'row',
+          'flex-row-reverse': direction === 'row-reverse',
+          'flex-col': direction === 'col',
+          'flex-col-reverse': direction === 'col-reverse',
+        },
+        {
+          'flex-1': flex === true,
+          'flex-auto': flex === 'auto',
+          'flex-initial': flex === 'initial',
+          'flex-none': flex === 'none',
+        },
+        {
+          'justify-start': justifyContent === 'start',
+          'justify-end': justifyContent === 'end',
+          'justify-center': justifyContent === 'center',
+          'justify-between': justifyContent === 'between',
+          'justify-around': justifyContent === 'around',
+          'justify-evenly': justifyContent === 'evenly',
+        },
+        {
+          'items-start': alignItems === 'start',
+          'items-end': alignItems === 'end',
+          'items-center': alignItems === 'center',
+          'items-baseline': alignItems === 'baseline',
+          'items-stretch': alignItems === 'stretch',
+        },
+        {
+          'h-full': fullHeight,
+          'w-full': fullWidth,
+        },
+        {
+          'flex-wrap': wrap === true,
+          'flex-wrap-reverse': wrap === 'reverse',
+        },
+        className
+      ),
+    [direction, flex, justifyContent, alignItems, fullHeight, fullWidth, wrap, className]
   )
 
   return (
