fb2f8442d37ebf822f6d08a355d1e3428cef8454
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-01-13 16:31:48 +0100

    Tweak note editor code language picker position

--- src/components/EditorCodeBlock.tsx
+++ src/components/EditorCodeBlock.tsx
@@ -34,11 +34,11 @@ const EditorCodeBlock: React.FC<NodeViewProps> = ({ editor, extension, node, upd
         tabIndex={tabIndex}
         onChange={onChangeLanguage}
         itemToString={getLanguageName}
-        className="absolute top-1 right-1"
-        menuClassName="rounded text-xs bg-zinc-500"
-        triggerPressedClassName="bg-zinc-300/50 hover:bg-zinc-300/50"
+        className="absolute bottom-full right-0"
+        triggerPressedClassName="bg-zinc-500 hover:bg-zinc-400"
         defaultItem={node.attrs.language ?? CODE_BLOCK_DEFAULT_LANGUAGE}
-        triggerClassName="shadow-none bg-zinc-500/50 hover:bg-zinc-400/50 px-2 py-1 text-xs rounded min-w-0 w-28"
+        menuClassName="rounded text-xs bg-zinc-600 -mt-0.5 rounded-t-none"
+        triggerClassName="shadow-none bg-zinc-700 hover:bg-zinc-600 px-2 py-1 text-xs rounded min-w-0 w-28 rounded-b-none"
       />
       <pre>
         <NodeViewContent as="code" />
