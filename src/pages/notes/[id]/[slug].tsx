59e7f2db5ed83b7ab201c851eeba71e44fca32aa
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-02-01 15:00:55 +0100

    Reset note component state on note/URL changes

--- src/pages/notes/[id]/[slug].tsx
+++ src/pages/notes/[id]/[slug].tsx
@@ -1,13 +1,16 @@
 import dynamic from 'next/dynamic'
 
 import Spinner from 'components/Spinner'
+import useContentId from 'hooks/useContentId'
 
 const Note = dynamic(import('components/Note'), {
   loading: () => <Spinner delay className="my-auto h-10 w-10 self-center" />,
 })
 
 const NotePage: Page = () => {
-  return <Note />
+  const contentId = useContentId()
+
+  return <Note key={contentId} />
 }
 
 export default NotePage
