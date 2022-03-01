8e4d71349159baa03d24e5574ec14e240d3be156
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-01 18:00:51 +0100

    Add search query cancellation

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -8,18 +8,22 @@ import { type SearchResulstData } from 'libs/db/file'
 export default function useSearch(enabled: boolean, query?: string) {
   const sanitizedQuery = query ? query.trim() : ''
 
-  return useInfiniteQuery(['search', query], ({ pageParam }) => getSearchResults(sanitizedQuery, pageParam), {
-    enabled: enabled && sanitizedQuery.length >= SEARCH_QUERY_MIN_LENGTH,
-    getNextPageParam: (lastPage, pages) => (lastPage.length === SEARCH_RESULT_LIMIT ? pages.length : undefined),
-  })
+  return useInfiniteQuery(
+    ['search', query],
+    ({ pageParam, signal }) => getSearchResults(signal, sanitizedQuery, pageParam),
+    {
+      enabled: enabled && sanitizedQuery.length >= SEARCH_QUERY_MIN_LENGTH,
+      getNextPageParam: (lastPage, pages) => (lastPage.length === SEARCH_RESULT_LIMIT ? pages.length : undefined),
+    }
+  )
 }
 
-function getSearchResults(query: string, page?: number) {
+function getSearchResults(signal: AbortSignal | undefined, query: string, page?: number) {
   const searchParams: SearchParamsOption = { q: query }
 
   if (page) {
     searchParams.page = page
   }
 
-  return client.get(`search`, { searchParams }).json<SearchResulstData>()
+  return client.get(`search`, { searchParams, signal }).json<SearchResulstData>()
 }
