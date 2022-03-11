dc87390515005a9988e1e13de72c43523bcbdee8
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-11 11:19:01 +0100

    Persist sidebar collapsed state

--- src/atoms/sidebar.ts
+++ src/atoms/sidebar.ts
@@ -1,6 +1,7 @@
 import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
-export const sidebarCollapsedAtom = atom(false)
+export const sidebarCollapsedAtom = atomWithStorage('haku.sidebarCollapsed', false)
 
 export const toggleSidebarCollapsedAtom = atom(null, (_get, set) => {
   set(sidebarCollapsedAtom, (prevSidebarCollapsed) => !prevSidebarCollapsed)
