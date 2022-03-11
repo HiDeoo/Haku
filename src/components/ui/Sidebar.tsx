4338481e6766f3736f42a974ada4da94eb1a6a9c
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-11 12:03:25 +0100

    Fix sidebar collapse button z-index issue

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -57,7 +57,7 @@ const Sidebar: React.FC = () => {
           iconClassName="w-5 h-5"
           onPress={toggleSidebarCollapsed}
           pressedClassName="bg-zinc-500 hover:bg-zinc-500"
-          className="fixed bottom-2 left-2 bg-zinc-900 p-2 shadow shadow-zinc-900 hover:bg-zinc-600 hover:text-blue-50 md:hidden"
+          className="fixed bottom-2 left-2 z-30 bg-zinc-900 p-2 shadow shadow-zinc-900 hover:bg-zinc-600 hover:text-blue-50 md:hidden"
         />
       ) : null}
     </>
