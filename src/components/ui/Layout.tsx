9f5d616544fad95373f864393300be340569a70d
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-14 16:26:56 +0100

    Fix sidebar border issue on mobile

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,12 +1,18 @@
+import { useAtomValue } from 'jotai/utils'
+
+import { sidebarCollapsedAtom } from 'atoms/collapsible'
 import Flex from 'components/ui/Flex'
 import Sidebar from 'components/ui/Sidebar'
 import clst from 'styles/clst'
 
 const Layout: React.FC<LayoutProps> = ({ children, sidebar }) => {
+  const sidebarCollapsed = useAtomValue(sidebarCollapsedAtom)
+
   const centered = sidebar ? undefined : 'center'
 
   const mainClasses = clst('overflow-hidden pwa:input-hover:border-t pwa:input-hover:border-t-zinc-700', {
-    'border-l border-l-zinc-600/30': sidebar,
+    'md:border-l md:border-l-zinc-600/30': sidebar,
+    'border-l border-l-zinc-600/30': sidebar && !sidebarCollapsed,
   })
 
   return (
