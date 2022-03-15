b23ebd1cce20ce13b71668a8972610405236a025
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-15 11:47:06 +0100

    Fix duplicated viewport meta issue

--- src/components/app/Title.tsx
+++ src/components/app/Title.tsx
@@ -12,6 +12,10 @@ const Title: React.FC<TitleProps> = ({ pageTitle }) => {
     <Head>
       <title>{title}</title>
       <meta name="theme-color" content={isOnDesktopPwa ? '#18181b' : '#27272a'} />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
+      />
     </Head>
   )
 }
