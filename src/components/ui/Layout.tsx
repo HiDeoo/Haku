984db64491e8b47bbb6112f41d1994f6e70fe209
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-15 17:24:54 +0100

    Add notch support to page without sidebar

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -8,17 +8,26 @@ import clst from 'styles/clst'
 const Layout: React.FC<LayoutProps> = ({ children, sidebar }) => {
   const sidebarCollapsed = useAtomValue(sidebarCollapsedAtom)
 
-  const centered = sidebar ? undefined : 'center'
+  const justifyCenter = sidebar ? undefined : 'center'
 
   const mainClasses = clst('overflow-hidden pwa:input-hover:border-t pwa:input-hover:border-t-zinc-700', {
     'md:border-l md:border-l-zinc-600/30': sidebar,
     'border-l border-l-zinc-600/30': sidebar && !sidebarCollapsed,
+    'supports-max:pb-[max(0px,env(safe-area-inset-bottom))] supports-max:pl-[max(0px,env(safe-area-inset-left))] supports-max:pr-[max(0px,env(safe-area-inset-right))]':
+      !sidebar,
   })
 
   return (
     <Flex fullHeight fullWidth>
       {sidebar ? <Sidebar /> : null}
-      <Flex as="main" justifyContent={centered} alignItems={centered} direction="col" flex className={mainClasses}>
+      <Flex
+        flex
+        as="main"
+        direction="col"
+        className={mainClasses}
+        alignItems={justifyCenter}
+        justifyContent={justifyCenter}
+      >
         {children}
       </Flex>
     </Flex>
