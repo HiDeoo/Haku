1ae79665c2961f13627ad559d4dd8c21636583c6
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-21 17:25:23 +0100

    Tweak headers & CSP

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@ import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })
 
+const commonHeaders = [
+  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+  { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+  { key: 'Cross-Origin-Resource-Policy', value: 'same-origin' },
+  { key: 'Referrer-Policy', value: 'same-origin' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'X-DNS-Prefetch-Control', value: 'on' },
+  { key: 'X-Frame-Options', value: 'DENY' },
+  { key: 'X-XSS-Protection', value: '1; mode=block' },
+]
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -11,6 +22,9 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  headers() {
+    return [{ source: '/:path*', headers: commonHeaders }]
   },
   reactStrictMode: true,
 }
