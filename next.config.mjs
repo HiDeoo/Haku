b6e5c5c8426724263cb017d2a7f30e72135d0e4e
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-04-25 11:24:44 +0200

    Block search indexing

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const commonHeaders = [
   { key: 'X-Content-Type-Options', value: 'nosniff' },
   { key: 'X-DNS-Prefetch-Control', value: 'on' },
   { key: 'X-Frame-Options', value: 'DENY' },
+  { key: 'X-Robots-Tag', value: 'noindex, nofollow, nosnippet, noarchive, noimageindex' },
   { key: 'X-XSS-Protection', value: '1; mode=block' },
 ]
 
