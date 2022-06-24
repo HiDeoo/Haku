7adb1499c660500e73bfa06a454dfee22648ba8f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-06-24 18:31:22 +0200

    Remove references to deprecated useUpdateAtom()

--- src/hooks/useFileHistory.ts
+++ src/hooks/useFileHistory.ts
@@ -1,4 +1,4 @@
-import { useUpdateAtom } from 'jotai/utils'
+import { useSetAtom } from 'jotai'
 
 import { fileHistoryAtom } from 'atoms/fileHistory'
 import useRouteChange from 'hooks/useRouteChange'
@@ -6,7 +6,7 @@ import useRouteChange from 'hooks/useRouteChange'
 const fileRegExp = /^\/(?:notes|todos)\/(?<id>c[\dA-Za-z]+)\/?/
 
 export function useFileHistory() {
-  const addToFileHistory = useUpdateAtom(fileHistoryAtom)
+  const addToFileHistory = useSetAtom(fileHistoryAtom)
 
   useRouteChange((url) => {
     const match = url.match(fileRegExp)
