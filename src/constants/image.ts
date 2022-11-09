c332db67c5535966d38d30066acbb4a56909585f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-11-09 12:49:17 +0100

    Update Next.js to v13

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,5 +1,7 @@
 export const IMAGE_SUPPORTED_TYPES = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'image/webp']
 
+// When updating this value, please also update the `config.api.bodyParser.sizeLimit` route configuration in
+// `src/pages/api/trpc/[trpc].ts` to match the new value as it cannot contain expressions.
 export const IMAGE_MAX_SIZE_IN_MEGABYTES = 4
 
 export const IMAGE_RESPONSIVE_BREAKPOINTS_IN_PIXELS = [320, 640, 960, 1280, 1600, 1920, 2240, 2560] as const
